3d32ddc95c9ab22e5782f0afd9dcb840002764c0
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2024-02-16 20:16:30 -0300

    add: Not Found & Internal Server error components, and Inital Transition for website component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -5,4 +5,8 @@ export { default as Courses } from "./home/Courses.jsx";
 export { default as Projects } from "./home/Projects.jsx";
 export { default as Services } from "./home/Services.jsx";
 export { default as Header } from "./home/Header.jsx";
+
 export { default as Footer } from "./common/footer/Footer.jsx";
+export { default as ServerError } from "./common/errors/ServerError.jsx";
+export { default as NotFound } from "./common/errors/NotFound.jsx";
+export { default as InitialTransition } from "./common/transitions/InitialTransition.jsx";
