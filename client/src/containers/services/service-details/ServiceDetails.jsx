66bc06ef7c0d088bb99c690aea623b2f5d7e7bab
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2024-04-17 01:10:45 -0300

    feat(client/ServiceDetails): Section with responsive styles partially done

--- client/src/containers/services/service-details/ServiceDetails.jsx
+++ client/src/containers/services/service-details/ServiceDetails.jsx
@@ -5,6 +5,7 @@ import { Helmet } from "react-helmet";
 import { Typography } from "../../../components";
 import { BsCheck } from "react-icons/bs";
 import { PersonalAvatar } from "../../../lib/images";
+import "../../../assets/styles/components/services/ServiceDetails.css";
 
 const ServiceDetails = () => {
   window.scrollTo(0, 0);
@@ -28,6 +29,7 @@ const ServiceDetails = () => {
     }
   }, [id, services, getServices]);
 
+  console.log(currentService);
   return (
     <>
       <Helmet>
@@ -48,8 +50,87 @@ const ServiceDetails = () => {
         " Servicio No Encontrado"
       ) : (
         <>
-          <section className="service-hero">hero</section>
-          <section className="service-work_modality">Work Modality</section>
+          <section className="service-hero">
+            <div className="service-hero__inner">
+              <div className="service-hero__description">
+                <Typography as="h1" variant="h1" color="white">
+                  {currentService?.title}
+                </Typography>
+                <Typography as="p" variant="small" color="lightGray">
+                  {currentService?.description}
+                </Typography>
+              </div>
+              <div className="service-hero__video">
+                {currentService && currentService.serviceIntroducingVideo ? (
+                  <video
+                    loop
+                    poster={currentService?.image}
+                    controls
+                    preload="auto"
+                  >
+                    <source
+                      src={currentService?.serviceIntroducingVideo}
+                      type="video/mp4"
+                    />
+                  </video>
+                ) : (
+                  <picture>
+                    <img
+                      src={currentService?.image}
+                      alt={`Imagen del curso "${currentService?.title}"`}
+                    />
+                  </picture>
+                )}
+              </div>
+            </div>
+          </section>
+          <section className="service-work_modality">
+            <div className="service-work_modality__inner">
+              <Typography as="h2" variant="h2" color="black">
+                Nuestras Modalidades de Trabajo
+              </Typography>
+
+              <div className="service-work_modality__wrapper">
+                {currentService && currentService.modules.length
+                  ? currentService.modules.map((modality) => (
+                      <article
+                        key={modality._id}
+                        className="service-work_modality__card"
+                      >
+                        <Typography as="h3" variant="h3" color="black">
+                          {modality.title}
+                        </Typography>
+                        <Typography as="p" variant="p" color="darkGray">
+                          {modality.description}
+                        </Typography>
+                      </article>
+                    ))
+                  : ""}
+              </div>
+            </div>
+          </section>
+          <section className="service-benefits">
+            <div className="service-benefits__inner">
+              <Typography as="h2" variant="h2" color="white">
+                Beneficios
+              </Typography>
+
+              <ul role="list">
+                {currentService && currentService.benefits
+                  ? currentService.benefits.map((benefit) => (
+                      <article key={benefit._id}>
+                        <Typography as="h3" variant="h3" color="black">
+                          {benefit.title}
+                        </Typography>
+                        <Typography as="p" variant="p" color="darkGray">
+                          {benefit.description}
+                        </Typography>
+                      </article>
+                    ))
+                  : ""}
+              </ul>
+            </div>
+          </section>
         </>
       )}
     </>
