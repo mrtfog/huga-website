8ea099a6a43bbc1e17cc504901fe4fd164f33784
mrtfog <fogliaccomartin3@gmail.com>
2024-04-19 13:27:13 -0300

    fix: navbar backgorund color stays if url path is distinct of /

--- client/src/lib/helpers.js
+++ client/src/lib/helpers.js
@@ -1,4 +1,10 @@
 window.addEventListener("scroll", () => {
-    const menu = document.getElementById('menu');
-    menu.classList.toggle('menu-scroll', window.scrollY>0);
-})
+  const path = window.location.pathname;
+  const menu = document.getElementById("menu");
+
+  if (path !== "/") {
+    menu.classList.toggle("menu-scroll");
+  } else {
+    menu.classList.toggle("menu-scroll", window.scrollY > 0);
+  }
+});
