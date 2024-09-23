f59d23be7ff0ea5f8fc098624e9fb49cdea53bd5
mrtfog <fogliaccomartin3@gmail.com>
2024-09-22 21:27:39 -0300

    add(server): work plan schema added

--- huga-sanity/schemas/index.js
+++ huga-sanity/schemas/index.js
@@ -3,5 +3,6 @@ import home from './home'
 import services from './services'
 import courses from './courses'
 import portfolio from './portfolio'
+import workPlan from './workPlan'
 
-export const schemaTypes = [socialMedia, home, services, courses, portfolio]
+export const schemaTypes = [socialMedia, home, services, courses, portfolio, workPlan]
---
+++ huga-sanity/schemas/workPlan.js
@@ -0,0 +1,122 @@
+import {orderRankField, orderRankOrdering} from '@sanity/orderable-document-list'
+import slugify from 'slugify'
+
+export default {
+  name: 'workPlan',
+  title: 'Planes de trabajo',
+  type: 'document',
+  orderings: [orderRankOrdering],
+  fields: [
+    orderRankField({type: 'workPlan'}),
+    {
+      name: 'available',
+      title: 'Disponible',
+      type: 'boolean',
+      options: {
+        layout: 'radio',
+        list: [
+          {title: 'Sí', value: true},
+          {title: 'No', value: false},
+        ],
+      },
+      validation: (Rule) => Rule.required().error('La disponibilidade es obligatoria'),
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        slugify: (input) => slugify(input, {lower: true, strict: true}),
+      },
+      validation: (Rule) => Rule.required().error('El Slug es obligatorio'),
+    },
+    {
+      name: 'image',
+      title: 'Imágen',
+      type: 'image',
+    },
+    {
+      name: 'title',
+      title: 'Título',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Descripción',
+      type: 'text',
+    },
+    {
+      name: 'objectivePublic',
+      title: 'Publico Objetivo',
+      type: 'text',
+    },
+    {
+      name: 'objective',
+      title: 'Objetivo',
+      type: 'text',
+    },
+    {
+      name: 'workModality',
+      title: 'Modalidad de trabajo',
+      type: 'text',
+    },
+    {
+      name: 'paymentModality',
+      title: 'Modalidad de pago',
+      type: 'text',
+    },
+    {
+      type: 'array',
+      name: 'includedServices',
+      title: 'Servicios incluídos',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'services'}],
+        },
+      ],
+    },
+    {
+      name: 'workPlan',
+      title: 'Plan de trabajo',
+      type: 'array',
+      of: [
+        {
+          type: 'object',
+          fields: [
+            {
+              name: 'title',
+              title: 'Título',
+              type: 'string',
+            },
+            {
+              name: 'description',
+              title: 'Descripción',
+              type: 'text',
+            },
+          ],
+          preview: {
+            select: {
+              title: 'title',
+              subtitle: 'description',
+            },
+          },
+        },
+      ],
+      options: {
+        limit: 3,
+      },
+    },
+  ],
+  preview: {
+    select: {
+      title: 'title',
+    },
+    prepare({title}) {
+      return {
+        title: title,
+      }
+    },
+  },
+}
